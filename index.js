fd72b31ffc925cc66a2c825919936951b19ee219
SudarshnaThakur15 <119600694+SudarshnaThakur15@users.noreply.github.com>
2025-03-29 21:49:32 +0530

    final touch v1

--- index.js
+++ index.js
@@ -6,7 +6,10 @@ const app = express();
 app.use(express.json()); // Middleware to parse JSON requests
 
 app.get('/', (req, res) => {
-  res.send('Hello World');
+  res.send(`
+    <h1>My Solution</h1>
+    <p>This is the server created. I am adding some random text here.</p>
+  `);
 });
 
 app.use('/', router); // Register routes
